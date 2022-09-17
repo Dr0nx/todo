3b67bf079d8c941592e628e736a1e75a4b2108d0
Andrey Bozhkov <kovbozh@gmail.com>
2022-09-17 14:08:21 +0300

    Update fill_db.py

--- users/management/commands/fill_db.py
+++ users/management/commands/fill_db.py
@@ -1,12 +1,8 @@
-import os
 import random
 import string
 
-from todo import settings
 from users.models import User
 from django.core.management.base import BaseCommand
-from django.utils.crypto import get_random_string
-import sqlite3
 
 
 class Command(BaseCommand):
@@ -15,7 +11,7 @@ class Command(BaseCommand):
         User.objects.all().delete()
 
         for i in range(3):
-            username = get_random_string(5).lower()
+            username = f'user{i + 1}'
             email = ''.join(random.choice(string.ascii_letters.lower()) for _ in range(5)) + '@gmail.com'
             User.objects.create_user(username=username, email=email, password='321')
 
