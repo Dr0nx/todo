0f77d863ebbec4017a6365fb4613b872e3e2ae49
Andrey Bozhkov <kovbozh@gmail.com>
2022-10-06 14:01:25 +0300

    Update some files

--- todo/urls.py
+++ todo/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from rest_framework.authtoken.views import obtain_auth_token
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
 
 from todoapp.views import ProjectModelViewSet, TodoModelViewSet
 from userapp.views import UserModelViewSet
@@ -31,4 +32,7 @@ urlpatterns = [
     path('api/', include(router.urls)),
     path('api-token/', obtain_auth_token),
     path('api-auth/', include('rest_framework.urls')),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
 ]
