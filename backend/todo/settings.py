bc3cbf7121e6ec6cbd622d5caf6095d24ac6795d
Andrey Bozhkov <kovbozh@gmail.com>
2022-11-08 18:18:15 +0300

    Update all

--- backend/todo/settings.py
+++ backend/todo/settings.py
@@ -139,8 +139,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'userapp.CustomUser'
 
 CORS_ALLOWED_ORIGINS = [
-    'http://localhost:3000',
-    'http://127.0.0.1:3000',
+    # 'http://localhost:3000',
+    # 'http://127.0.0.1:3000',
     'http://192.168.0.3:3000',
 ]
 
