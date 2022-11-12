5a414fb7c489551d81bebac71519da61087eed89
Andrey Bozhkov <kovbozh@gmail.com>
2022-11-12 16:26:17 +0300

    Update all

--- backend/userapp/management/commands/fill_db.py
+++ backend/userapp/management/commands/fill_db.py
@@ -9,15 +9,15 @@ class Command(BaseCommand):
     def handle(self, *args, **kwargs):
         CustomUser.objects.all().delete()
 
-        CustomUser.objects.create_superuser(username='Администратор 1', first_name='Администратор', last_name='',
+        CustomUser.objects.create_superuser(username='Администратор', first_name='Администратор', last_name='',
                                      email='kovbozh@gmail.com', password='123')
-        CustomUser.objects.create_user(username='Владелец проектов 1', is_staff=True, first_name='Владелец проектов', last_name='',
+        CustomUser.objects.create_user(username='Владелец_проектов_1', is_staff=True, first_name='Владелец проектов', last_name='',
                                  email='kovbozh1@gmail.com', password='123')
-        CustomUser.objects.create_user(username='Владелец проектов 2', is_staff=True, first_name='Владелец проектов', last_name='',
+        CustomUser.objects.create_user(username='Владелец_проектов_2', is_staff=True, first_name='Владелец проектов', last_name='',
                                  email='kovbozh2@gmail.com', password='123')
-        CustomUser.objects.create_user(username='Разработчик 1', is_staff=True, first_name='Разработчик', last_name='',
+        CustomUser.objects.create_user(username='Разработчик_1', is_staff=True, first_name='Разработчик', last_name='',
                                  email='kovbozh3@gmail.com', password='123')
-        CustomUser.objects.create_user(username='Разработчик 2', is_staff=True, first_name='Разработчик', last_name='',
+        CustomUser.objects.create_user(username='Разработчик_2', is_staff=True, first_name='Разработчик', last_name='',
                                  email='kovbozh4@gmail.com', password='123')
         CustomUser.objects.create_superuser(username='dr0n', first_name='Андрей', last_name='Божков',
                                       email='dr0nx@yandex.ru', password='123')
