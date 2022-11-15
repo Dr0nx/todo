17e2ceb7096d807c65e8633d4cd8126e1645c839
Andrey Bozhkov <kovbozh@gmail.com>
2022-11-15 07:52:23 +0300

    Update settings.py

--- backend/todo/settings.py
+++ backend/todo/settings.py
@@ -145,8 +145,6 @@ AUTH_USER_MODEL = 'userapp.CustomUser'
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:3000',
     'http://127.0.0.1:3000',
-    # 'http://127.0.0.1:8000',
-    # 'http://127.0.0.1',
 ]
 
 REST_FRAMEWORK = {
